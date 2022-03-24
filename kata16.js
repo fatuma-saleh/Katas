b8ea6d15db5f99e08c581f7881c38998348b2a93
Fatuma <sfatex@gmail.com>
2022-03-23 22:49:01 -0400

    change string in to title case

--- kata16.js
+++ kata16.js
@@ -38,6 +38,31 @@ const makeCase = function (input, Case) {
        }
        return output;
        break;
+
+       case "kebab":
+      for (let i = 0; i < input.length; i++) {
+        if (input[i] === " ") {
+          output += "-"
+          
+        } else {
+          output = output + input[i];
+        }
+      }
+      return output;
+      break;
+
+      case "title":
+        output= input[0].toUpperCase() 
+      for (let i = 1; i < input.length; i++) {
+        if (input[i] === " ") {
+          output=output + " " + input[i + 1].toUpperCase();
+          i++
+        } else {
+          output = output + input[i];
+        }
+      }
+      return output;
+      break;
     default:
       break;
   }
@@ -46,8 +71,8 @@ const makeCase = function (input, Case) {
 console.log(makeCase("this is a string", "camel"));
 console.log(makeCase("this is a string", "pascal"));
 console.log(makeCase("this is a string", "snake"));
-// console.log(makeCase("this is a string", "kebab"));
-// console.log(makeCase("this is a string", "title"));
+console.log(makeCase("this is a string", "kebab"));
+console.log(makeCase("this is a string", "title"));
 // console.log(makeCase("this is a string", "vowel"));
 // console.log(makeCase("this is a string", "consonant"));
 // console.log(makeCase("this is a string", ["upper", "snake"]));
