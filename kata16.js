099b0839504a30d21ffad73e6d07d346fa95f830
Fatuma <sfatex@gmail.com>
2022-03-24 00:57:01 -0400

    convert string to an array of multiple cases

--- kata16.js
+++ kata16.js
@@ -1,83 +1,109 @@
-const makeCase = function (input, Case) {
+const makeCase = function(input, Case) {
   // Put your solution here
   let output = "";
-  switch (Case) {
+  if (!Array.isArray(Case)) {
+    switch (Case) {
     case "camel":
       for (let i = 0; i < input.length; i++) {
         if (input[i] === " ") {
           output += input[i + 1].toUpperCase();
-          i++
+          i++;
         } else {
           output = output + input[i];
         }
       }
       return output;
-      break;
+      
 
-      case "pascal":
-       output= input[0].toUpperCase()
+    case "pascal":
+      output = input[0].toUpperCase();
       for (let i = 1; i < input.length; i++) {
         if (input[i] === " ") {
           output += input[i + 1].toUpperCase();
-          i++
+          i++;
         } else {
           output = output + input[i];
         }
       }
       return output;
-      break;
-      case "snake":
-        
-       for (let i = 0; i <input.length; i++) {
-         if (input[i] === " ") {
-           output += '_';
-     
-         } else {
-           output = output + input[i];
-         }
-       }
-       return output;
-       break;
-
-       case "kebab":
+      
+    case "snake":
       for (let i = 0; i < input.length; i++) {
         if (input[i] === " ") {
-          output += "-"
-          
+          output += "_";
         } else {
           output = output + input[i];
         }
       }
       return output;
-      break;
-
-      case "title":
-        output= input[0].toUpperCase() 
+      
+    case "kebab":
+      for (let i = 0; i < input.length; i++) {
+        if (input[i] === " ") {
+          output += "-";
+        } else {
+          output = output + input[i];
+        }
+      }
+      return output;
+      
+    case "title":
+      output = input[0].toUpperCase();
       for (let i = 1; i < input.length; i++) {
         if (input[i] === " ") {
-          output=output + " " + input[i + 1].toUpperCase();
-          i++
+          output = output + " " + input[i + 1].toUpperCase();
+          i++;
         } else {
           output = output + input[i];
         }
       }
       return output;
-      break;
+      
 
-      case "vowel":
-        for (let i = 0; i < input.length; i++) {
-          if (input[i] === "a" ||input[i] === "e" ||input[i] === "i"||input[i] === "o" ||input[i] === "u" ) {
-            output += input[i].toUpperCase()
-            
-          } else {
-            output = output + input[i];
-          }
+    case "vowel":
+      for (let i = 0; i < input.length; i++) {
+        if (
+          input[i] === "a" ||
+          input[i] === "e" ||
+          input[i] === "i" ||
+          input[i] === "o" ||
+          input[i] === "u"
+        ) {
+          output += input[i].toUpperCase();
+        } else {
+          output = output + input[i];
         }
-        return output;
-        break;
+      }
+      return output;
+     
 
+    case "consonant":
+      for (let i = 0; i < input.length; i++) {
+        if (input[i] !== "a" && input[i] !== "e" &&
+          input[i] !== "i" &&
+          input[i] !== "o" &&
+          input[i] !== "u") {
+          output += input[i].toUpperCase();
+        } else {
+          output = output + input[i];
+        }
+      }
+      return output;
+     
     default:
       break;
+    }
+  } else if (Case[0] === "upper" && Case[1] === "snake") {
+    output += input[0].toUpperCase();
+    for (let i = 1; i < input.length; i++) {
+      if (input[i] === " ") {
+        output = output + "_" + input[i + 1].toUpperCase();
+        i++;
+      } else {
+        output = output + input[i];
+      }
+    }
+    return output;
   }
 };
 
@@ -87,8 +113,8 @@ console.log(makeCase("this is a string", "snake"));
 console.log(makeCase("this is a string", "kebab"));
 console.log(makeCase("this is a string", "title"));
 console.log(makeCase("this is a string", "vowel"));
-// console.log(makeCase("this is a string", "consonant"));
-// console.log(makeCase("this is a string", ["upper", "snake"]));
+console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", ["upper", "snake"]));
 
 //expected output
 // thisIsAString
