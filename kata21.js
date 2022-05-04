4c55c3f6111d300d7ce8f5e1d381899c97f735d3
Fatuma <sfatex@gmail.com>
2022-05-04 12:43:10 -0400

    implement printAll

--- kata21.js
+++ kata21.js
@@ -1,58 +1,59 @@
 //implement a function biggestFollower() which returns the name of the individual who follows the most people.
 
 const biggestFollower = function (data) {
-  let max = 0,temp=""
+  let max = 0,
+    temp = "";
   for (const persons in data) {
-   // for(const p in data[persons]){
-      //console.log(data[persons].name)
-     if (data[persons].follows.length > max) {
-      max = data[persons].follows.length
-      temp = data[persons].name
+    // for(const p in data[persons]){
+    //console.log(data[persons].name)
+    if (data[persons].follows.length > max) {
+      max = data[persons].follows.length;
+      temp = data[persons].name;
     }
     //}
-  
-    
-    }
-  
-  return temp
-};
+  }
 
+  return temp;
+};
 
 //Implement mostPopular() which returns the name of the most popular (most followed) individual.
 
-const mostPopular = function(data){
-  let countObj = {}, max = 0,famous = ""            
-  for(const persons in data){
-    let p = data[persons]
-    for(let follow of p.follows){
-      if(countObj[follow]){
-        countObj[follow]++
-      }else{
-        countObj[follow]=1
+const mostPopular = function (data) {
+  let countObj = {},
+    max = 0,
+    famous = "";
+  for (const persons in data) {
+    let p = data[persons];
+    for (let follow of p.follows) {
+      if (countObj[follow]) {
+        countObj[follow]++;
+      } else {
+        countObj[follow] = 1;
       }
     }
   }
-for(let mostPop in countObj){
-               
-  if(countObj[mostPop]>max){
-    max = countObj[mostPop]
-    famous = mostPop
+  for (let mostPop in countObj) {
+    if (countObj[mostPop] > max) {
+      max = countObj[mostPop];
+      famous = mostPop;
+    }
   }
-}
-  
 
-
-
-
-
-  return famous
-}
-
+  return famous;
+};
 
 //Implement printAll() which outputs a list of everyone and for each of them, the names of who they follow and who follows them.
-const printAll = function(){
-  
-}
+const printAll = function (data) {
+  let individualDetails = []
+  for (const persons in data) {
+    individualDetails.push(data[persons].name)
+    individualDetails.push(data[persons].follows)
+    
+    
+  }
+  return individualDetails
+};
+
 const data = {
   f01: {
     name: "Alice",
@@ -88,3 +89,4 @@ const data = {
 
 console.log(biggestFollower(data));
 console.log(mostPopular(data));
+console.log(printAll(data))
